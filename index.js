75d1ec2f6d3f79ab72439210341c404b0cd766d9
Daniel Schmitz <danieljfa@gmail.com>
2021-09-10 19:39:10 -0300

    date utils

--- index.js
+++ index.js
@@ -22,3 +22,20 @@ exports.hasKey = (obj, key) => {
     return true
   })
 }
+
+/**
+ * Recebe uma data americana e retorna no formato BR
+ * @param {String} date Uma data no formato YYYY-MM-DD
+ * @returns {String} Uma data no formato DD/MM/YYYY
+ */
+exports.formatDateToBr = (date) => {
+  if (!date) return null
+  const [year, month, day] = date.split("-")
+  return `${day}/${month}/${year}`
+}
+
+exports.formatDateToEn = (date) => {
+  if (!date) return null
+  const [day, month, year] = date.split("/")
+  return `${year}-${month}-${day}`
+}
